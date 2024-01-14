2d2501b4049bba45d012c4462fddadb550a24ea4
Aleksandar Haralampiev <alek.h.haralampiev@gmail.com>
2024-01-14 17:05:43 +0200

    domashno 5

---
+++ C programming/9 klas/domashni/domashno 5/util.c
@@ -0,0 +1,10 @@
+#include "util.h"
+#include <stdio.h>
+
+void print_usage() {
+    printf("Usage: program <num1> <num2>\n");
+}
+
+void print_overflow() {
+    printf("Overflow occurred\n");
+}
---
+++ C programming/9 klas/domashni/domashno 5/util.h
@@ -0,0 +1,5 @@
+#ifndef UTIL_H
+#define UTIL_H
+void print_usage();
+void print_overflow();
+#endif
