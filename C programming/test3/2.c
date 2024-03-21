38d3c609c954134c700270f149f63663b48bf59a
Aleksandar Haralampiev <alek.h.haralampiev@gmail.com>
2024-03-21 11:04:36 +0200

    test

---
+++ C programming/test3/2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double pow2(double num) {
+    return pow(num, 2);
+}
+
+double pow4(double num) {
+    return pow(num, 4);
+}
+
+void tar(double* arr, int size, double (*func)(double)) {
+    for(int i = 0; i < size; i++) {
+        arr[i] = func(arr[i]);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    double* arr = (double*)malloc(n * sizeof(double));
+    if(arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    printf("Enter the elements: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%lf", &arr[i]);
+    }
+
+    int choice;
+    printf("Enter 0 for pow2 or 1 for pow4: ");
+    scanf("%d", &choice);
+
+    if(choice == 0) {
+        tar(arr, n, pow2);
+    } else if(choice == 1) {
+        tar(arr, n, pow4);
+    }
+
+    printf("Result array: ");
+    for(int i = 0; i < n; i++) {
+        printf("%lf ", arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
+
+    return 0;
+}
